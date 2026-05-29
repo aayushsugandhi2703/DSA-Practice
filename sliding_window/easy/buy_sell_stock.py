3c46bbc8da3970cbe82367728fcecdf7c1ac96ef
Aayush Sugandhi <aayushsugandhi@Aayushs-MacBook-Pro.local>
2026-05-29 10:12:25 +0530

    added more problems

--- sliding_window/easy/buy_sell_stock.py
+++ sliding_window/easy/buy_sell_stock.py
@@ -1,11 +1,11 @@
 # Given the list of stocks prices, find the maximum profit you can make by buying and selling one share of the stock. 
 # You must buy before you sell.
 
-def max_profit(prices):
+def max_profit_using_two_pointers(prices):
     l, r = 0, 1
     best = 0
     while r < len(prices):
-        if prices[l]< prices[r]:
+        if prices[l] < prices[r]:
             price = prices[r] - prices[l]
             best = max(best, price)
         else:
@@ -16,7 +16,7 @@ def max_profit(prices):
 
 def main():
     prices = [7,1,5,3,6,4]
-    result = max_profit(prices)
+    result = max_profit_using_two_pointers(prices)
     print(f"The maximum profit you can make by buying and selling one share of the stock is: {result}")
 
 if __name__ == "__main__":
